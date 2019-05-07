b3344958fafb29b35839151749651c08972b9615
amunozhe <amunozhe@redhat.com>
2019-05-07 11:47:21 +0200

    Replace booster by example
    Related to: SB-1345

--- src/main/java/dev/snowdrop/example/exception/NotFoundException.java
+++ src/main/java/dev/snowdrop/example/exception/NotFoundException.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package io.openshift.booster.exception;
+package dev.snowdrop.example.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
