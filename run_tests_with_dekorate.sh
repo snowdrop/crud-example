222d362ad2d9f0d7d643bf3a055321f33ca2c0ab
Jose Carvajal <josecarvajalhilario@gmail.com>
2021-12-09 09:12:56 +0100

    Setup apps in s2i script (#86)

--- run_tests_with_dekorate.sh
+++ run_tests_with_dekorate.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# deploy database: won't be needed in Dekorate 1.7.0. Related to https://github.com/dekorateio/dekorate/pull/798
+# deploy database
 oc create -f .openshiftio/database.yaml
 timeout 300s bash -c 'while [[ $(oc get pod -o json | jq  ".items[] | select(.metadata.name | contains(\"my-database\"))  | .status  " | jq -rs "sort_by(.startTme) | last | .phase") != "Running" ]]; do sleep 20; done; echo ""'
 
