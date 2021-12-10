5992380d2d616de6a24df2b5b546c668220a4547
Jose <josecarvajalhilario@gmail.com>
2021-12-10 10:36:28 +0100

    Change Postgresql Image to quay.io/bitnami/postgresql:13.4.0

--- run_tests_with_dekorate.sh
+++ run_tests_with_dekorate.sh
@@ -2,7 +2,9 @@
 
 # deploy database
 oc create -f .openshiftio/database.yaml
+sleep 30 # needed in order to bypass the 'Pending' state
 timeout 300s bash -c 'while [[ $(oc get pod -o json | jq  ".items[] | select(.metadata.name | contains(\"my-database\"))  | .status  " | jq -rs "sort_by(.startTme) | last | .phase") != "Running" ]]; do sleep 20; done; echo ""'
+oc logs $(oc get pod -o json | jq  ".items[] | select(.metadata.name | contains(\"my-database\"))  | .metadata  " | jq -rs "sort_by(.startTme) | last | .name")
 
 # Run OpenShift Tests
 ./mvnw -s .github/mvn-settings.xml clean verify -Popenshift,openshift-it
--- run_tests_with_s2i.sh
+++ run_tests_with_s2i.sh
@@ -4,7 +4,9 @@ SOURCE_REPOSITORY_REF=${2:-sb-2.4.x}
 
 # deploy database
 oc create -f .openshiftio/database.yaml
+sleep 30 # needed in order to bypass the 'Pending' state
 timeout 300s bash -c 'while [[ $(oc get pod -o json | jq  ".items[] | select(.metadata.name | contains(\"my-database\"))  | .status  " | jq -rs "sort_by(.startTme) | last | .phase") != "Running" ]]; do sleep 20; done; echo ""'
+oc logs $(oc get pod -o json | jq  ".items[] | select(.metadata.name | contains(\"my-database\"))  | .metadata  " | jq -rs "sort_by(.startTme) | last | .name")
 
 # deploy application
 oc create -f .openshiftio/application.yaml
