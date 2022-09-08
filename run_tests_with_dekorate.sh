3a8d694e38c6a4e277408fbab6e9dfd6dbc37e21
Antonio Costa <antcosta@redhat.com>
2022-09-08 15:47:41 +0200

    feat: improved dekorate bash to override any parameter

--- run_tests_with_dekorate.sh
+++ run_tests_with_dekorate.sh
@@ -1,25 +1,4 @@
 #!/usr/bin/env bash
 
-source scripts/waitFor.sh
-
-# deploy database
-oc create -f .openshiftio/database.yaml
-if [[ $(waitFor "my-database" "app") -eq 1 ]] ; then
-  echo "Database failed to deploy. Aborting"
-  exit 1
-fi
-
-SB_VERSION_SWITCH=""
-
-while getopts v: option
-do
-    case "${option}"
-        in
-        v)SB_VERSION_SWITCH="-Dspring-boot.version=${OPTARG}";;
-    esac
-done
-
-echo "SB_VERSION_SWITCH: ${SB_VERSION_SWITCH}"
-
 # Run OpenShift Tests
-eval "./mvnw -s .github/mvn-settings.xml clean verify -Popenshift,openshift-it ${SB_VERSION_SWITCH}"
+eval "./mvnw -s .github/mvn-settings.xml clean verify -Popenshift,openshift-it  $@"
