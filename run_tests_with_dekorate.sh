73c98fb633f05a832d4e3935effccb87794abc88
Antonio Costa <antcosta@redhat.com>
2022-09-12 10:45:24 +0200

    fix: recover the database creation on the dekorate script

--- run_tests_with_dekorate.sh
+++ run_tests_with_dekorate.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 
+# deploy database
+oc create -f .openshiftio/database.yaml
+if [[ $(waitFor "my-database" "app") -eq 1 ]] ; then
+  echo "Database failed to deploy. Aborting"
+  exit 1
+fi
+
 # Run OpenShift Tests
 eval "./mvnw -s .github/mvn-settings.xml clean verify -Popenshift,openshift-it $@"
