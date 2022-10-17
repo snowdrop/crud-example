4dd2de59d728920d6901dea0593f184d7f4fa2ab
Jose <josecarvajalhilario@gmail.com>
2022-10-17 07:24:59 +0200

    Bump dekorate to 3.0.2

--- run_tests_with_helm_in_ocp.sh
+++ run_tests_with_helm_in_ocp.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 SOURCE_REPOSITORY_URL=${1:-https://github.com/snowdrop/crud-example}
-SOURCE_REPOSITORY_REF=${2:-sb-2.5.x}
+SOURCE_REPOSITORY_REF=${2:-sb-2.7.x}
 S2I_BUILDER_IMAGE_REPO=registry.access.redhat.com/ubi8/openjdk-11
 S2I_BUILDER_IMAGE_TAG=1.14
 
