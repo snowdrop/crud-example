4f593ba1a32cb5bf1de75b4ad4087f663800769c
Jose <josecarvajalhilario@gmail.com>
2022-10-18 09:45:34 +0200

    Downgrade Dekorate to 2.11.3

--- run_tests_with_dekorate_in_k8s.sh
+++ run_tests_with_dekorate_in_k8s.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 CONTAINER_REGISTRY=${1:-localhost:5000}
 K8S_NAMESPACE=${2:-k8s}
+MAVEN_OPTS=${3:-}
 
 source scripts/waitFor.sh
 
@@ -14,5 +15,5 @@ if [[ $(waitFor "my-database" "app") -eq 1 ]] ; then
 fi
 
 # deploy application and run tests
-./mvnw -s .github/mvn-settings.xml clean verify -Pkubernetes,kubernetes-it -Ddekorate.docker.registry=$CONTAINER_REGISTRY -Dkubernetes.namespace=$K8S_NAMESPACE -Ddekorate.push=true
+./mvnw -s .github/mvn-settings.xml clean verify -Pkubernetes,kubernetes-it -Ddekorate.docker.registry=$CONTAINER_REGISTRY -Dkubernetes.namespace=$K8S_NAMESPACE -Ddekorate.push=true $MAVEN_OPTS
 
